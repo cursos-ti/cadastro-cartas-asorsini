18c3d488b315bf36541e45cf3b44ba94943afb4f
Anderson Orsini <asorsini@gmail.com>
2026-03-14 20:34:08 +0000

    Final da parte 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,7 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
     
-  char estado1, estado2;
+  char estado1[1], estado2[1];
   char codcarta1[3], codcarta2[3];
   char cidade1[10], cidade2[10];
   int populacao1, populacao2;
@@ -17,14 +17,21 @@ int main() {
 
   // Área para entrada de dados
 
-    printf("Digite o código do estado da carta1:) estado1");
-      scanf("%char", &estado1);
-
-
-
-
-
-
+    printf("Digite o código do estado da carta1: ");
+      scanf("%char", estado1);
+    printf("Digite o código da carta: ");
+      scanf("%char", codcarta1);
+    printf("Digite a cidade: ");
+      scanf("%char", cidade1);
+    printf("Digite a população: ");
+      scanf("%d", populacao1);
+    printf("Digite a áres em KM² :");
+      scanf("%.2f", area1);
+    printf("Digite o PIB da cidade: ");
+      scanf("%.2f, pib1");
+    printf("Digite o número de pontos turísticos: ");
+      scanf("%d, pontosturisticos1");
+      
 
   // Área para exibição dos dados da cidade
 
