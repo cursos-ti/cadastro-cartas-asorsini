a20d09de3b1afc52f4edf793bb072a071ca037e6
Anderson Orsini <asorsini@gmail.com>
2026-03-14 15:52:43 -0300

    Primeiro teste

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,7 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+char = 
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
