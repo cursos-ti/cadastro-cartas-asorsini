eb74a16432b483e7b1675742406da77fa23f519c
Anderson Orsini <asorsini@gmail.com>
2026-03-14 23:23:56 +0000

    Erro 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,7 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
     
-  char estado1, estado2;
+  char estadoa[3], estadob[3];
   char codcarta1[4], codcarta2[4];
   char cidade1[10], cidade2[10];
   float populacao1, populacao2;
@@ -20,7 +20,7 @@ int main() {
   // Carta 1
   printf("Digite os dados da primeira carta:\n");
     printf("Digite o código do estado da carta 1: ");
-      scanf("%s", estado1);    
+      scanf("%s", estadoa);    
     printf("Digite o código da carta: ");
       scanf("%s", codcarta1);
     printf("Digite a cidade: ");
@@ -37,7 +37,7 @@ int main() {
   // Carta 2
     printf("Digite os dados da segunda carta:\n");
     printf("Digite o código do estado da carta: ");
-    scanf("%s", estado2);
+    scanf("%s", estadob);
     printf("Digite o código da carta: ");
       scanf("%s", codcarta2);
     printf("Digite a cidade: ");
@@ -50,10 +50,15 @@ int main() {
       scanf("%f", &pib2);
     printf("Digite o número de pontos turísticos: ");
       scanf("%d", &pontosturisticos2);
-      
+    printf("\n")  
   // Área para exibição dos dados da cidade
 
-    printf("O Código do estado 1 é: %d\n"), estado1; 
+    printf("O Código do estado da carta 1 é: %s. E da carta 2 é: %s\n", estadoa, estadob);
+    printf("O Código da carta 1 é: %s. E da carta 2 é: %s\n", codcarta1, codcarta2);
+    printf("A cidade da carta 1 é: %s. E da carta 2 é: %s\n", cidade1, cidade2);
+    printf("A população da carta 1 é: %.2f. E da carta 2 é: %.2f\n", populacao1, populacao2);
+    printf("A áreda da cidade da carta 1 é: %.2f KM². E da carta 2 é: %.2f KM²\n", area1, area2);
+
 return 0;
 } 
 
